5ae2f3f4ab439cab2f8c9f3e8eb80e1260371927
Jasper <jasper@haenenweb.nl>
2020-10-02 14:58:51 +0200

    Initialized app

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1 class="epic">{{ greeting }}</h1>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            greeting: "Hello Vue!"
+        };
+    }
+};
+</script>
+
+<style scoped>
+    .epic {
+        color: greenyellow;
+    }
+</style>
