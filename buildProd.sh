edfddc536f49497bb3513003346ebfd253d9bdfc
jhaenen <jasper@haenenweb.nl>
2020-10-09 16:48:22 +0200

    Added favicons

--- buildProd.sh
+++ buildProd.sh
@@ -4,8 +4,6 @@ service taken stop
 
 npm run build
 cp ./server.js $targetDir/server.js
-cp ./prod_dist/index.html $targetDir/dist/index.html
-cp ./prod_dist/main.js $targetDir/dist/main.js
-cp ./prod_dist/style.css $targetDir/dist/style.css
+cp ./prod_dist/* $targetDir/dist/
 
 service taken restart
